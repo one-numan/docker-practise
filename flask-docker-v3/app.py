064aa7d2aad33d8162b5c90d616cba867212c1dd
Mohd Numan <48924562+one-numan@users.noreply.github.com>
2026-01-20 14:15:22 +0530

    Docker | Running ngnix Flask and PostGre

---
+++ flask-docker-v3/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),        # must be "db"
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=5432                         # 👈 IMPORTANT LINE
+    )
+
+@app.route("/")
+def home():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT 'DB connected 🚀';")
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return result[0]
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
